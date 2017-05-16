a074d149fa7acc1398f679a5d48b34beb976f0ea
Hiroki Omae <hiroki@unity3d.com>
2017-05-17 07:28:56 +0900

    initial commit

---
+++ AssetBundleManager/Editor/AssetBundleManagerControlPanel.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AssetBundles {
+    public class AssetBundleManagerControlPanel : EditorWindow {
+
+        private static AssetBundleManagerControlPanel s_window;
+
+//        private Texture2D m_localServerButtonIcon;
+//        private Texture2D m_clearCacheOnPlayButtonIcon;
+        private int m_loadFrom;
+
+        internal class Config {
+            internal static readonly string LOCAL_SERVER_BUTTON_ICON = "Assets/AssetBundleManager/Editor/Graphics/localserver.psd";
+            internal static readonly string CLEAR_CACHE_BUTTON_ICON = "Assets/AssetBundleManager/Editor/Graphics/clearcache.psd";
+            internal static readonly string[] LOADFROM = new string[] {
+            "Regular Bundle Path",
+            "GraphTool Output"
+            };
+        }
+
+        [MenuItem("Window/AssetBundle Manager/Open Control Panel...", false, 4)]
+        public static void Open () {
+            GetWindow<AssetBundleManagerControlPanel>();
+        }
+
+        public static Texture2D LoadTextureFromFile(string path) {
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(File.ReadAllBytes(path));
+            return texture;
+        }
+
+        private void Init() {
+            this.titleContent = new GUIContent("ABMgr Control");
+//            m_localServerButtonIcon = 
+//                LoadTextureFromFile (Config.LOCAL_SERVER_BUTTON_ICON);
+//
+//            m_clearCacheOnPlayButtonIcon = 
+//                LoadTextureFromFile (Config.CLEAR_CACHE_BUTTON_ICON);
+
+            m_loadFrom = (AssetBundleManager.UseGraphToolBundle) ? 1 : 0;
+        }
+
+
+        public void OnEnable () {
+            Init();
+        }
+
+        public void OnFocus() {
+        }
+
+        public void OnDisable() {
+        }
+
+        public void OnGUI () {
+
+            using (new EditorGUILayout.VerticalScope()) {
+
+                bool newClearCache = GUILayout.Toggle(AssetBundleManager.CleanCacheOnPlay, 
+                    "Clear Cache On Play");
+                bool newLocalServer = GUILayout.Toggle(LaunchAssetBundleServer.IsRunning(), 
+                    "Local Server");
+
+                bool newSimulationMode = GUILayout.Toggle(AssetBundleManager.SimulateAssetBundleInEditor, 
+                    "Simulation mode");
+                
+                if (newClearCache != AssetBundleManager.CleanCacheOnPlay) {
+                    AssetBundleManager.CleanCacheOnPlay = newClearCache;
+                }
+                if (newSimulationMode != AssetBundleManager.SimulateAssetBundleInEditor) {
+                    AssetBundleManager.SimulateAssetBundleInEditor = newSimulationMode;
+                }
+                if (newLocalServer != LaunchAssetBundleServer.IsRunning()) {
+                    LaunchAssetBundleServer.ToggleLocalAssetBundleServer ();
+                }
+
+                GUILayout.Space(12f);
+
+                int idx = EditorGUILayout.Popup (m_loadFrom, Config.LOADFROM);
+                if (idx != m_loadFrom) {
+                    m_loadFrom = idx;
+                    if (LaunchAssetBundleServer.IsRunning ()) {
+                        LaunchAssetBundleServer.ToggleLocalAssetBundleServer ();
+                    }
+                    AssetBundleManager.UseGraphToolBundle = (idx == 1);
+                    m_loadFrom = idx;
+                }
+
+                var url = EditorGUILayout.TextField("Server URL", AssetBundleManager.BaseDownloadingURL);
+                if (url != AssetBundleManager.BaseDownloadingURL) {
+                    AssetBundleManager.BaseDownloadingURL = url;
+                }
+                GUILayout.Space(8f);
+
+                EditorGUILayout.HelpBox (LaunchAssetBundleServer.GetServerArgs(), MessageType.Info);
+
+                GUILayout.Space(8f);
+            }
+        }
+    }
+}
---
+++ AssetBundleManager/Editor/LaunchAssetBundleServer.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System;
+using System.Net;
+using System.Threading;
+using UnityEditor.Utils;
+
+namespace AssetBundles
+{
+	internal class LaunchAssetBundleServer : ScriptableSingleton<LaunchAssetBundleServer>
+	{
+		const string kLocalAssetbundleServerMenu = "Assets/AssetBundles/Local AssetBundle Server";
+
+		[SerializeField]
+		private int 	m_ServerPID = 0;
+
+        [SerializeField]
+        private string     m_args;
+
+        [MenuItem (kLocalAssetbundleServerMenu)]
+		public static void ToggleLocalAssetBundleServer ()
+		{
+			bool isRunning = IsRunning();
+			if (!isRunning)
+			{
+				Run ();
+			}
+			else
+			{
+				KillRunningAssetBundleServer ();
+			}
+		}
+
+		[MenuItem (kLocalAssetbundleServerMenu, true)]
+		public static bool ToggleLocalAssetBundleServerValidate ()
+		{
+			bool isRunnning = IsRunning ();
+			Menu.SetChecked(kLocalAssetbundleServerMenu, isRunnning);
+			return true;
+		}
+
+		public static bool IsRunning ()
+		{
+			if (instance.m_ServerPID == 0)
+				return false;
+
+			var process = Process.GetProcessById (instance.m_ServerPID);
+			if (process == null)
+				return false;
+
+			return !process.HasExited;
+		}
+
+        public static string GetServerArgs () {
+            return instance.m_args;
+        }
+
+		static void KillRunningAssetBundleServer ()
+		{
+			// Kill the last time we ran
+			try
+			{
+				if (instance.m_ServerPID == 0)
+					return;
+
+				var lastProcess = Process.GetProcessById (instance.m_ServerPID);
+				lastProcess.Kill();
+				instance.m_ServerPID = 0;
+			}
+			catch
+			{
+			}
+		}
+		
+		static void Run ()
+		{
+			string pathToAssetServer = Path.Combine(Application.dataPath, "AssetBundleManager/Editor/AssetBundleServer.exe");
+			string pathToApp = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/'));
+	
+			KillRunningAssetBundleServer();
+			
+			BuildScript.WriteServerURL();
+
+			string bundleFolder = AssetBundleManager.UseGraphToolBundle ? "GraphToolBundles" : "AssetBundles";
+			
+			string args = Path.Combine (pathToApp, bundleFolder);
+
+			if(!Directory.Exists(args)) {
+                instance.m_args = "Directory does not exist. Build asset bundles first and create directory to run local server:" + args;
+                UnityEngine.Debug.LogError(instance.m_args);
+				return;
+			}
+
+			#if UNITY_2017_1_OR_NEWER
+			string monoProfile = "4.5";
+			#elif UNITY_5_5_OR_NEWER
+			string monoProfile = "v4.0.30319";
+			#else
+			string monoProfile = "4.0";
+			#endif
+
+			args = string.Format("\"{0}\" {1}", args, Process.GetCurrentProcess().Id);
+			ProcessStartInfo startInfo = ExecuteInternalMono.GetProfileStartInfoForMono(MonoInstallationFinder.GetMonoInstallation("MonoBleedingEdge"), monoProfile, pathToAssetServer, args, true);
+			startInfo.WorkingDirectory = Path.Combine(System.Environment.CurrentDirectory, bundleFolder);
+			startInfo.UseShellExecute = false;
+			Process launchProcess = Process.Start(startInfo);
+			if (launchProcess == null || launchProcess.HasExited == true || launchProcess.Id == 0)
+			{
+				//Unable to start process
+				UnityEngine.Debug.LogError ("Unable Start AssetBundleServer process");
+                instance.m_args = "Not running.";
+			}
+			else
+			{
+				UnityEngine.Debug.LogFormat("Local Server started with arg:{0}", args);
+				//We seem to have launched, let's save the PID
+				instance.m_ServerPID = launchProcess.Id;
+                instance.m_args = args;
+			}
+		}
+	}
+}
